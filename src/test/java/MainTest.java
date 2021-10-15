856d92bc8c6006454704de3c0da8b9c0fd95ad41
Priyashna <raj07priyashna@gmail.com>
2021-10-15 21:44:29 +1200

    Build basic structure

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Absar
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        Main.main(null);
+    }
+
+}
